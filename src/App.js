e8eeb81a70b6f1585791fe7a7b928aba66c4b151
Alexandria Ellis <allstarcoding777@outlook.com>
2023-04-09 13:02:41 -0500

    new file structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Alexandria Ellis Portfolio</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Under Construction</h2>
-        <h2>Check back soon!</h2>
-      </header>
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
+
+// import logo from './logo.svg';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Alexandria Ellis Portfolio</h1>
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h2>Under Construction</h2>
+//         <h2>Check back soon!</h2>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+// import dependencies
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// render the App component to the DOM, the DOM is the browser
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // root is a variable that holds the value of the DOM, ReactDOM.createRoot is an API allows us to render our application to the DOM
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+
+//   // structure of the application
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
