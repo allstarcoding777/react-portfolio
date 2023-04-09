61315cb71062e334c93f7a448c216f0040a6eebe
Alexandria Ellis <allstarcoding777@outlook.com>
2023-04-09 13:21:38 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
-import './App.css';
 
 const App = () => <PortfolioContainer />;
 
 export default App;
-
-// import logo from './logo.svg';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>Alexandria Ellis Portfolio</h1>
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <h2>Under Construction</h2>
-//         <h2>Check back soon!</h2>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 // import dependencies
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // render the App component to the DOM, the DOM is the browser
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// // root is a variable that holds the value of the DOM, ReactDOM.createRoot is an API allows us to render our application to the DOM
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-
-//   // structure of the application
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
