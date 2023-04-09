d27d80760b6c227f025b7500320beb2776ff6d40
Alexandria Ellis <allstarcoding777@outlook.com>
2023-04-09 14:29:08 -0500

    css styling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './assets/css/pages.css'
+const developerImg = require('./assets/images/developer-in-office.png');
 
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Alexandria Ellis Portfolio</h1>
       <p>
-       home content
+      <img className='img' src={developerImg} alt='developer-in-office' />
       </p>
     </div>
   );
