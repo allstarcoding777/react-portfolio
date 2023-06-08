35802f5276e8b05ad6407c3ea889a00197ef56f7
Alexandria Ellis <allstarcoding777@outlook.com>
2023-06-08 00:31:16 -0500

    mywork styling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './assets/css/pages.css'
-import MaterialIcon, {} from 'material-icons-react';
 const profileImg = require('./assets/images/photo.jpg');
 
-
-// link to my work page
 export default function Home() {
   return (
     <body className='home-body'>
@@ -14,7 +11,6 @@ export default function Home() {
       <h1 className='col-5'>Hello, I am Alex<br></br><span>a Full-Stack <br></br>Web Developer</span></h1>
       <section className='col'>
       <div className='col'>
-      {/* <a className = 'my-work' href="/MyWork" >View My Work &nbsp;&nbsp;<MaterialIcon icon="arrow_forward" /></a> */}
       <a className = 'github' href="https://github.com/allstarcoding777?tab=repositories" target="_blank" rel="noreferrer">GitHub</a>
       <a className = 'linkedin' href="https://www.linkedin.com/in/alexandria-ellis-98589b1a4" target="_blank" rel="noreferrer">LinkedIn</a>
       </div>
--- src/components/pages/MyWork.js
+++ src/components/pages/MyWork.js
@@ -8,53 +8,31 @@ const notesPhoto = require('./assets/images/notes.png');
 
 export default function Contact() {
   return (
-      <main className ='my-work'>
-        <div className="container">
-  <div className="row">
-    <div className="col">
-    <a a className='work-title' href="https://realtime-chat-777.herokuapp.com/" target="_blank" rel="noreferrer"><h2>Realtime Chat</h2></a>
-            <a href="https://realtime-chat-777.herokuapp.com/" target="_blank" rel="noreferrer"><img className='work-img' src={chatPhoto} alt="realtime chat"></img></a>
-    </div>
-    <div className="col">
-      <a className='work-title' href="https://allstarcoding777.github.io/weather-dashboard/" target="_blank" rel="noreferrer"><h2>Weather Dashboard</h2></a>
-            <a href="https://allstarcoding777.github.io/weather-dashboard/" target="_blank" rel="noreferrer"><img className='work-img' src={weatherPhoto} alt="realtime chat"></img></a>
-    </div>
-  </div>
-</div>
-
-<div className="row">
-    <div className="col">
-      <a a className='work-title' href='https://glittery-madeleine-c76884.netlify.app/' target="blank" rel="noreferrer"><h2>Workout Tracker</h2></a>
-    <a href="https://glittery-madeleine-c76884.netlify.app/" target="blank" rel="noreferrer"><img className='work-img' src={workoutPhoto} alt="translate"></img></a>
-    </div>
-    <a className='work-title' href="https://note-taker777.herokuapp.com/" target="blank" rel="noreferrer"><h2>Note Taker</h2></a>
-    <a href="https://note-taker777.herokuapp.com/" target="blank" rel="noreferrer"><img className='note-img' src={notesPhoto} alt="translate"></img></a>
-    <div className="col">
-    </div>
-  </div>
-
-  <div className="row">
-    <div className="col">
-      <a a className='work-title' href='https://allstarcoding777.github.io/en2es/' target="blank" rel="noreferrer"><h2>English to Spanish Translator</h2></a>
-    <a href="https://allstarcoding777.github.io/en2es/" target="blank" rel="noreferrer"><img className='work-img' src={translatePhoto} alt="translate"></img></a>
-    </div>
-    <a className='work-title' href="https://allstarcoding777.github.io/work-day-scheduler/" target="blank" rel="noreferrer"><h2>Work Day Scheduler</h2></a>
-    <a href="https://allstarcoding777.github.io/work-day-scheduler/" target="blank" rel="noreferrer"><img className='work-img' src={calendarPhoto} alt="translate"></img></a>
-    <div className="col">
-    </div>
-  </div>
-
-  
-
-        {/* <h2 className='col-6 col-sm-4'><a href="https://realtime-chat-777.herokuapp.com/" target="_blank">Realtime Chat</a></h2>
-            <a href="https://realtime-chat-777.herokuapp.com/" target="_blank"><img className='work-img' src={chatPhoto} alt="realtime chat"></img></a>
-  
-        <h2 className='col-6 col-sm-4'><a href="https://allstarcoding777.github.io/en2es/" target="_blank">Translate</a></h2>
-            <a href="https://allstarcoding777.github.io/en2es/" target="blank"><img className='work-img' src={translatePhoto} alt="translate"></img></a>
-        <h2 className='col-6'><a href="https://allstarcoding777.github.io/weather-dashboard/" target="_blank">Weather Dashboard</a></h2>
-            <a href="https://allstarcoding777.github.io/weather-dashboard/" target="blank"><img src={weatherPhoto} alt="weather dashboard"></img></a> */}
-
-      </main>
-
+    <main className='my-work'>
+        <div className="col">
+          <a a className='work-title' href="https://realtime-chat-777.herokuapp.com/" target="_blank" rel="noreferrer"><h2>Realtime Chat</h2></a>
+          <a href="https://realtime-chat-777.herokuapp.com/" target="_blank" rel="noreferrer"><img className='work-img' src={chatPhoto} alt="realtime chat"></img></a>
+        </div>
+        <div className="col">
+          <a className='work-title' href="https://allstarcoding777.github.io/weather-dashboard/" target="_blank" rel="noreferrer"><h2>Weather Dashboard</h2></a>
+          <a href="https://allstarcoding777.github.io/weather-dashboard/" target="_blank" rel="noreferrer"><img className='work-img' src={weatherPhoto} alt="realtime chat"></img></a>
+        </div>
+        <div className="col">
+          <a a className='work-title' href='https://glittery-madeleine-c76884.netlify.app/' target="blank" rel="noreferrer"><h2>Workout Tracker</h2></a>
+          <a href="https://glittery-madeleine-c76884.netlify.app/" target="blank" rel="noreferrer"><img className='work-img' src={workoutPhoto} alt="translate"></img></a>
+        </div>
+        <div className="col">
+        <a className='work-title' href="https://note-taker777.herokuapp.com/" target="blank" rel="noreferrer"><h2>Note Taker</h2></a>
+        <a href="https://note-taker777.herokuapp.com/" target="blank" rel="noreferrer"><img className='note-img work-img' src={notesPhoto} alt="translate"></img></a>
+        </div>
+        <div className="col">
+          <a a className='work-title' href='https://allstarcoding777.github.io/en2es/' target="blank" rel="noreferrer"><h2>English to Spanish Translator</h2></a>
+          <a href="https://allstarcoding777.github.io/en2es/" target="blank" rel="noreferrer"><img className='work-img' src={translatePhoto} alt="translate"></img></a>
+        </div>
+        <div className="col">
+        <a className='work-title' href="https://allstarcoding777.github.io/work-day-scheduler/" target="blank" rel="noreferrer"><h2>Work Day Scheduler</h2></a>
+        <a href="https://allstarcoding777.github.io/work-day-scheduler/" target="blank" rel="noreferrer"><img className='work-img' src={calendarPhoto} alt="translate"></img></a>
+        </div>
+    </main>
   );
 }
