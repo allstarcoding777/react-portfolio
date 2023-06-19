be7ddbae49ed5a4fa52ca35672fa5a685ed58d2d
Alexandria Ellis <allstarcoding777@outlook.com>
2023-06-19 14:14:34 -0500

    added quiz project

--- src/components/pages/MyWork.js
+++ src/components/pages/MyWork.js
@@ -1,10 +1,10 @@
 import React from 'react';
 const chatPhoto = require('./assets/images/chat.png');
 const translatePhoto = require('./assets/images/translate.png');
-const weatherPhoto = require('./assets/images/weather.png');
 const calendarPhoto = require('./assets/images/calender.png');
 const workoutPhoto = require('./assets/images/workout.png');
 const notesPhoto = require('./assets/images/notes.png');
+const quizPhoto = require('./assets/images/quiz.png');
 
 export default function Contact() {
   return (
@@ -14,8 +14,8 @@ export default function Contact() {
           <a href="https://realtime-chat-777.herokuapp.com/" target="_blank" rel="noreferrer"><img className='work-img' src={chatPhoto} alt="realtime chat"></img></a>
         </div>
         <div className="col">
-          <a className='work-title' href="https://allstarcoding777.github.io/weather-dashboard/" target="_blank" rel="noreferrer"><h2>Weather Dashboard</h2></a>
-          <a href="https://allstarcoding777.github.io/weather-dashboard/" target="_blank" rel="noreferrer"><img className='work-img' src={weatherPhoto} alt="realtime chat"></img></a>
+          <a className='work-title' href="https://allstarcoding777.github.io/beginner-coding-quiz/" target="_blank" rel="noreferrer"><h2>Coding Quiz</h2></a>
+          <a href="https://allstarcoding777.github.io/beginner-coding-quiz/" target="_blank" rel="noreferrer"><img className='work-img' src={quizPhoto} alt="realtime chat"></img></a>
         </div>
         <div className="col">
           <a a className='work-title' href='https://workout-tracker-777.herokuapp.com/' target="blank" rel="noreferrer"><h2>Workout Tracker</h2></a>
