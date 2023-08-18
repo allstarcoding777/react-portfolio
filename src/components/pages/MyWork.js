123d2c3c660927f09d88d65dd322507b390129bc
Alexandria Ellis <allstarcoding777@outlook.com>
2023-08-18 17:31:48 -0500

    my work layout update

--- src/components/pages/MyWork.js
+++ src/components/pages/MyWork.js
@@ -14,12 +14,12 @@ export default function Contact() {
           <a href="https://realtime-chat-777.herokuapp.com/" target="_blank" rel="noreferrer"><img className='work-img' src={chatPhoto} alt="realtime chat"></img></a>
         </div>
         <div className="col">
-          <a className='work-title' href="https://allstarcoding777.github.io/beginner-coding-quiz/" target="_blank" rel="noreferrer"><h2>Coding Quiz</h2></a>
-          <a href="https://allstarcoding777.github.io/beginner-coding-quiz/" target="_blank" rel="noreferrer"><img className='work-img' src={quizPhoto} alt="realtime chat"></img></a>
-        </div>
-        <div className="col">
           <a a className='work-title' href='https://workout-tracker-777.herokuapp.com/' target="blank" rel="noreferrer"><h2>Workout Tracker</h2></a>
           <a href="https://workout-tracker-777.herokuapp.com/" target="blank" rel="noreferrer"><img className='work-img' src={workoutPhoto} alt="translate"></img></a>
+        </div>
+        <div className="col">
+          <a className='work-title' href="https://allstarcoding777.github.io/beginner-coding-quiz/" target="_blank" rel="noreferrer"><h2>Coding Quiz</h2></a>
+          <a href="https://allstarcoding777.github.io/beginner-coding-quiz/" target="_blank" rel="noreferrer"><img className='work-img' src={quizPhoto} alt="realtime chat"></img></a>
         </div>
         {/* <div className="col">
         <a className='work-title' href="https://note-taker777.herokuapp.com/" target="blank" rel="noreferrer"><h2>Note Taker</h2></a>
